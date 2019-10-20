0712fdb88126eeb409f965985e873be921fb46a9
Daniel Persson <bighatsmallfeet@gmail.com>
2019-10-20 22:23:05 +0200

    fixed index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
-export * from './FluentAws';
-export * from './FluentAwsConfig';
-
 export * from './autoScaling/AutoScaling';
 export * from './autoScaling/AutoScalingGroup';
 export * from './autoScaling/AutoScalingGroupCollection';
 
+export * from './cf/CloudFormation';
+export * from './cf/CloudFormationStack';
+export * from './cf/CloudFormationStackCollection';
+
+export * from './dynamoDb/DynamoDb';
+export * from './dynamoDb/DynamoDbTable';
+export * from './dynamoDb/DynamoDbTableCollection';
+
 export * from './ec2/Ec2';
+export * from './ec2/Ec2AccountAttributes';
 export * from './ec2/Ec2Instance';
 export * from './ec2/Ec2InstanceCollection';
 
@@ -19,9 +25,18 @@ export * from './ecs/EcsTaskCollection';
 
 export * from './iam/IamRole';
 
+export * from './kms/Kms';
+export * from './kms/KmsAlias';
+export * from './kms/KmsAliasCollection';
+export * from './kms/KmsKey';
+export * from './kms/KmsKeyCollection';
+
 export * from './route53/Route53';
 export * from './route53/Route53HealthCheck';
 export * from './route53/Route53HealthCheckCollection';
+export * from './route53/Route53HostedZone';
+export * from './route53/Route53HostedZoneCollection';
+export * from './route53/Route53RecordSetCollection';
 
 export * from './s3/S3';
 export * from './s3/S3Bucket';
@@ -29,6 +44,9 @@ export * from './s3/S3BucketCollection';
 export * from './s3/S3Object';
 export * from './s3/S3ObjectCollection';
 
-export * from './dynamoDb/DynamoDb';
-export * from './dynamoDb/DynamoDbTable';
-export * from './dynamoDb/DynamoDbTableCollection';
+export * from './ssm/SystemsManager';
+export * from './ssm/SystemsManagerParameter';
+export * from './ssm/SystemsManagerParameterCollection';
+
+export * from './FluentAws';
+export * from './FluentAwsConfig';
